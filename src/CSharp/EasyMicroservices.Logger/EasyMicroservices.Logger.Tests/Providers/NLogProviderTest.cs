fc24af3b2a8e57936bae20a9584efb00e1ce4520
Ali Yousefi <ali.visual.studio@gmail.com>
2023-10-18 18:36:13 +0330

    Add test cases

---
+++ src/CSharp/EasyMicroservices.Logger/EasyMicroservices.Logger.Tests/Providers/NLogProviderTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMicroservices.Logger.NLog.Providers;
+using NLog;
+
+namespace EasyMicroservices.Logger.Tests.Providers
+{
+    public class NLogProviderTest : BaseProviderTest
+    {
+        public NLogProviderTest() : base(new NLogProvider(LogManager.GetCurrentClassLogger()))
+        {
+        }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Logger/EasyMicroservices.Logger.Tests/Providers/SerilogProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using EasyMicroservices.Logger.Serilog.Providers;
+using Serilog;
+
+namespace EasyMicroservices.Logger.Tests.Providers
+{
+    public class SerilogProviderTest : BaseProviderTest
+    {
+        public SerilogProviderTest() : base(new SerilogProvider(new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("serilog.txt")))
+        {
+        }
+    }
+}
